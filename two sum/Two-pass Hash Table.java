9b187f083d1c01cc0214ce4113cf9da42557e2a4
Xu-DongHui <45915266+Xu-DongHui@users.noreply.github.com>
2018-12-29 20:50:42 +0800

    Create Two-pass Hash Table.java

---
+++ two sum/Two-pass Hash Table.java
@@ -0,0 +1,16 @@
+public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        map.put(nums[i], i);
+    }
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement) && map.get(complement) != i) {
+            return new int[] { i, map.get(complement) };
+        }
+    }
+    throw new IllegalArgumentException("No two sum solution");
+}
+//Time complexity : O(n)
+//Space complexity: O(n)
+//reduce the look up time from O(n)to O(1) by trading space for speed
