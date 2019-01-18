3848ddf399efba8a2ce25ebf3d7e59438d95362d
Xu-DongHui <45915266+Xu-DongHui@users.noreply.github.com>
2019-01-18 11:26:08 +0800

    Update VerticalScanning.java

--- LongestCommonPrefix/VerticalScanning.java
+++ LongestCommonPrefix/VerticalScanning.java
@@ -3,6 +3,7 @@ public String longestCommonPrefix(String[] strs) {
     for (int i = 0; i < strs[0].length() ; i++){
         char c = strs[0].charAt(i);
         for (int j = 1; j < strs.length; j ++) {
+            //compare characters from top to bottom on the same column (same character index of the strings) before moving on to the next column.
             //the iterate will be stopped, if i is larger than the length of one of strs, or, the common prefix is finished.
             if (i == strs[j].length() || strs[j].charAt(i) != c)
                 return strs[0].substring(0, i);             
