9b8c6f99ad97602c3f1d5483b99d43c86451669e
Xu-DongHui <45915266+Xu-DongHui@users.noreply.github.com>
2019-01-18 14:29:41 +0800

    Create BinarySearch.java

---
+++ LongestCommonPrefix/BinarySearch.java
@@ -0,0 +1,29 @@
+public String longestCommonPrefix(String[] strs) {
+    if (strs == null || strs.length == 0)
+        return "";
+    int minLen = Integer.MAX_VALUE;
+    for (String str : strs)
+        minLen = Math.min(minLen, str.length());
+    int low = 1;
+    int high = minLen;
+    //seperate minnum string into two parts
+    while (low <= high) {
+        int middle = (low + high) / 2;
+        //varify the left part 
+        if (isCommonPrefix(strs, middle))
+            //multiply the character of left part to suit the common prefix of all the strings
+            low = middle + 1;
+        else
+            high = middle - 1;
+    }
+    return strs[0].substring(0, (low + high) / 2);
+}
+
+private boolean isCommonPrefix(String[] strs, int len){
+    //testfy the whether the common prefix exists in the first len characters of all the strings.
+    String str1 = strs[0].substring(0,len);
+    for (int i = 1; i < strs.length; i++)
+        if (!strs[i].startsWith(str1))
+            return false;
+    return true;
+}
