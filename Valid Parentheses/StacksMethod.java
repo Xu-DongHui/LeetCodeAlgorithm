aa4ba6b5ec3bf78a8c4901123e2d9e2cd6d63258
Xu-DongHui <45915266+Xu-DongHui@users.noreply.github.com>
2019-02-20 19:16:43 +0800

    Create StacksMethod.java

---
+++ Valid Parentheses/StacksMethod.java
@@ -0,0 +1,45 @@
+class Solution {
+
+  // Hash table that takes care of the mappings.
+  private HashMap<Character, Character> mappings;
+
+  // Initialize hash map with mappings. This simply makes the code easier to read.
+  public Solution() {
+    this.mappings = new HashMap<Character, Character>();
+    //通过hashmap的键值对来实现对应括号的匹配查询
+    //hashmap的key是唯一的，通过key可以查询到对应的值
+    //hashmap能够实现switch匹配的功能
+    this.mappings.put(')', '(');
+    this.mappings.put('}', '{');
+    this.mappings.put(']', '[');
+  }
+
+  public boolean isValid(String s) {
+
+    // Initialize a stack to be used in the algorithm.
+    Stack<Character> stack = new Stack<Character>();
+
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+
+      // If the current character is a closing bracket.
+      if (this.mappings.containsKey(c)) {//hashmap匹配字符
+
+        // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+        //栈可能为空，因此需要进行判断，如果为空则用一个伪值来代替
+        char topElement = stack.empty() ? '#' : stack.pop();
+
+        // If the mapping for this bracket doesn't match the stack's top element, return false.
+        if (topElement != this.mappings.get(c)) {
+          return false;
+        }
+      } else {
+        // If it was an opening bracket, push to the stack.
+        stack.push(c);
+      }
+    }
+
+    // If the stack still contains elements, then it is an invalid expression.
+    return stack.isEmpty();
+  }
+}
